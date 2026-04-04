45995ee203cb03d6c1264b938522a82c6f07b33c
Pooja Gangasagar <poojagangasagar@email.com>
2026-04-04 15:13:52 +0530

    added kubernetes pod

--- import java.util.java
+++ import java.util.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
-class Addition  {
+import java.util.Scanner; 
+ public class Main {
     public static void main(String[]args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Enter first number");
         int num1 = sc.nextInt();
         System.out.println("Enter second number");
         int num2 = sc.nextInt();
-        System.out.println("Sum: " + (num1 + num2));
+        System.out.println("sum: " +(num1 + num2));
     }
+
+    
 }
