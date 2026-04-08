8978b0520e389e73a7a683eacd476e4a14df0958
Pooja Gangasagar <poojagangasagar@email.com>
2026-04-08 23:39:05 +0530

    availiablity zone added

--- docker/terraform/sample.tf
+++ docker/terraform/sample.tf
@@ -6,6 +6,7 @@ ami = ami-0ec10929233384c7f
 instance_type = "t3.micro"
 key_name ="id_rsa"
 vpc_security_group_ids = ["sg-053d5a946de99ca5b",]
+availability_zone = "us-east-1a"
 tags ={
 name = pooja
 Name = "myinstance"
